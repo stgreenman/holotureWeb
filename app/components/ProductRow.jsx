994ce4c7e8b5350343c599808343ca4a3aa80146
Logan Bentley <loganbentley22@gmail.com>
2016-11-07 08:48:04 -0700

    Moved default product row stuff into props and state and added some styling to handle different states

--- app/components/ProductRow.jsx
+++ app/components/ProductRow.jsx
@@ -1,13 +1,47 @@
 var React = require('react');
 
 var ProductRow = React.createClass({
+  getInitialState: function() {
+    return {
+      isSelected: false,
+    };
+  },
+  getDefaultProps: function() {
+    return {
+      imageSrc: "https://ak1.ostkcdn.com/images/products/11706636/P18629716.jpg",
+      productTitle: "Couch",
+      productDescription: "This couch is an excellent choice because it's just so darn comfortable"
+    }
+  },
+  onClick: function() {
+    var {isSelected} = this.state;
+    var setSelected = true;
+    if (isSelected) {
+      setSelected = false;
+    }
+
+    this.setState({
+      isSelected: setSelected
+    });
+  },
   render: function() {
+    var {imageSrc, productTitle, productDescription} = this.props;
+    var {isSelected} = this.state;
+
+    var containerClassName;
+    if (isSelected) {
+      containerClassName = "selected-row";
+    }
+    else {
+      containerClassName = "row";
+    }
+
     return (
-      <div className="row">
-        <img src="https://ak1.ostkcdn.com/images/products/11706636/P18629716.jpg" className="image"></img>
+      <div className={containerClassName} onClick={this.onClick}>
+        <img src={imageSrc} className="image"></img>
         <div className="descriptive-text">
-          <h5>Couch</h5>
-          <p>This couch is an excellent choice because it's just so darn comfortable</p>
+          <h5>{productTitle}</h5>
+          <p>{productDescription}</p>
         </div>
       </div>
     );
