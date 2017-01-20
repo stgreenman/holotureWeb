70fc08f1e040aed689382184fab2d32b0d194fe5
Logan Bentley <Logan Bentley>
2017-01-20 08:54:04 -0700

    Changed out furniture

--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -20,37 +20,51 @@ var catalogProducts = [
                 },
                 {
                   id: 3,
-                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/LoveSeat/loveseat.png",
-                  title: "Other Love seat",
-                  description: "This love seat is a perfect choice as an accent chair.",
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/AllureChair.JPG",
+                  title: "Allure Chair",
+                  description: "This chair is so very alluring.",
                   selected: false
                 },
                 {
                   id: 4,
-                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/LoveSeat/loveseat.png",
-                  title: "One more Love seat",
-                  description: "This love seat is a perfect choice as an accent chair.",
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/GliderRecliner.JPG",
+                  title: "Glider Recliner",
+                  description: "Glide in this recliner all day long.",
                   selected: false
                 },
                 {
                   id: 5,
-                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/WestElmCouch/westelm.png",
-                  title: "Another Westelm Sofa",
-                  description: "This couch is an excellent choice because it's just so darn comfortable!",
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/GordonAccentChair.JPG",
+                  title: "Gordon Accent Chair",
+                  description: "This chair will be the perfect accent!",
                   selected: false
                 },
                 {
                   id: 6,
-                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/WestElmCouch/westelm.png",
-                  title: "Sofa",
-                  description: "This couch is an excellent choice because it's just so darn comfortable!",
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/MotionSofa.JPG",
+                  title: "Motion Sofa",
+                  description: "Dont get motion sickness in this motion sofa!",
                   selected: false
                 },
                 {
                   id: 7,
-                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/WestElmCouch/westelm.png",
-                  title: "Westelm Sofa",
-                  description: "This couch is an excellent choice because it's just so darn comfortable!",
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/SmallFullSleeper.JPG",
+                  title: "Small Full Sleeper",
+                  description: "This sleepr may be small but it is definitely full!",
+                  selected: false
+                },
+                {
+                  id: 8,
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/PippaAccentChair.JPG",
+                  title: "Pippa Accent Chair",
+                  description: "A great accent chair for the living room!",
+                  selected: false
+                },
+                {
+                  id: 9,
+                  imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/OxfordRecliner.JPG",
+                  title: "Oxford Recliner",
+                  description: "High class recliner for the living room. So comfortable!",
                   selected: false
                 },
               ];
