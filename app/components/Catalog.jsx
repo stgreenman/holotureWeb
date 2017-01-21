8a3ce5b7f73d6cf2f28f61b0c46eddacb81cb474
Logan Bentley <Logan Bentley>
2017-01-20 19:40:59 -0700

    Added initial filtering capabilities

--- app/components/Catalog.jsx
+++ app/components/Catalog.jsx
@@ -9,63 +9,72 @@ var catalogProducts = [
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/WestElmCouch/westelm.png",
                   title: "Westelm Sofa",
                   description: "This couch is an excellent choice because it's just so darn comfortable!",
-                  selected: false
+                  selected: false,
+                  categoryId: 1,
                 },
                 {
                   id: 2,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/LoveSeat/loveseat.png",
                   title: "Love seat",
                   description: "This love seat is a perfect choice as an accent chair.",
-                  selected: false
+                  selected: false,
+                  categoryId: 0,
                 },
                 {
                   id: 3,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/AllureChair.JPG",
                   title: "Allure Chair",
                   description: "This chair is so very alluring.",
-                  selected: false
+                  selected: false,
+                  categoryId: 0,
                 },
                 {
                   id: 4,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/GliderRecliner.JPG",
                   title: "Glider Recliner",
                   description: "Glide in this recliner all day long.",
-                  selected: false
+                  selected: false,
+                  categoryId: 0,
                 },
                 {
                   id: 5,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/GordonAccentChair.JPG",
                   title: "Gordon Accent Chair",
                   description: "This chair will be the perfect accent!",
-                  selected: false
+                  selected: false,
+                  categoryId: 0,
                 },
                 {
                   id: 6,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/MotionSofa.JPG",
                   title: "Motion Sofa",
                   description: "Dont get motion sickness in this motion sofa!",
-                  selected: false
+                  selected: false,
+                  categoryId: 1,
                 },
                 {
                   id: 7,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/SmallFullSleeper.JPG",
                   title: "Small Full Sleeper",
                   description: "This sleepr may be small but it is definitely full!",
-                  selected: false
+                  selected: false,
+                  categoryId: 1,
                 },
                 {
                   id: 8,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/PippaAccentChair.JPG",
                   title: "Pippa Accent Chair",
                   description: "A great accent chair for the living room!",
-                  selected: false
+                  selected: false,
+                  categoryId: 0,
                 },
                 {
                   id: 9,
                   imageSrc: "https://s3-us-west-2.amazonaws.com/holoture/OxfordRecliner.JPG",
                   title: "Oxford Recliner",
                   description: "High class recliner for the living room. So comfortable!",
-                  selected: false
+                  selected: false,
+                  categoryId: 0,
                 },
               ];
 var folderProducts = [];
@@ -142,16 +151,19 @@ var Catalog = React.createClass({
     this.setState({ open: false });
   },
   render: function() {
-    var {catalogProducts, folderProducts, open, selectedProduct } = this.state;
+    const {catalogProducts, folderProducts, open, selectedProduct } = this.state;
+    const { selectedCategoryId }  = this.props;
 
     var productList = catalogProducts.map(function(product) {
-      return <ProductCard
-                key={product.id}
-                product={product}
-                type="catalog"
-                className="product-card"
-                openModal={this.handleOpenModal}
+      if (product.categoryId === selectedCategoryId || selectedCategoryId === undefined) {
+        return <ProductCard
+                  key={product.id}
+                  product={product}
+                  type="catalog"
+                  className="product-card"
+                  openModal={this.handleOpenModal}
                 />;
+      }
     }, this);
 
     return (
--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -11,14 +11,46 @@ var Main = React.createClass({
 		// console.log('sideBarVisible', this.state.sideBarVisible);
 		this.setState({
 			sideBarVisible: !this.state.sideBarVisible,
-			sideBarButtonActive: !this.state.sideBarButtonActive
+			sideBarButtonActive: !this.state.sideBarButtonActive,
+			selectedCategoryId: undefined,
 		});
+	},
+	handleOnClick: function(categoryId) {
+		var { productCategories } = this.state;
+		productCategories = productCategories.map(function(category) {
+			if (category.id === categoryId) {
+				category.active = true;
+				this.setState({ selectedCategoryId: category.id });
+			}
+			else {
+				category.active = false;
+			}
+			return category;
+		}, this);
+		this.setState({ productCategories: productCategories });
 	},
 	getInitialState: function() {
 		return {
 			itemCount: 0,
 			sideBarVisible: false,
 			sideBarButtonActive: false,
+			productCategories: [
+				{
+					id: 0,
+					name: "Chairs",
+					active: false,
+				},
+				{
+					id: 1,
+					name: "Couches",
+					active: false,
+				},
+				{
+					id: 2,
+					name: "Tables",
+					active: false,
+				}
+			],
 		}
 	},
 	onAdded: function(item) {
@@ -37,7 +69,18 @@ var Main = React.createClass({
 		}, this);
 	},
 	render: function() {
-		var {itemCount, sideBarVisible, hideOrShow, sideBarButtonActive} = this.state;
+		var {itemCount, sideBarVisible, hideOrShow, sideBarButtonActive, productCategories, selectedCategoryId} = this.state;
+
+		var menuItemList = productCategories.map(function(category) {
+			return <Menu.Item
+								active={category.active}
+								name={category.name}
+								onClick={() => this.handleOnClick(category.id)}
+								key={category.id}
+							>
+										{ category.name }
+						 </Menu.Item>
+		}, this);
 
 		return (
 			<div>
@@ -45,20 +88,12 @@ var Main = React.createClass({
 				<Button toggle onClick={this.toggleSideBarVisibility} active={sideBarButtonActive}><Icon disabled name='list layout'></Icon></Button>
 				<Sidebar.Pushable as={Segment}>
 					<Sidebar as={Menu} animation='push' width='thin' visible={sideBarVisible} vertical inverted>
-						<Menu.Item name='couches'>
-							Couches
-						</Menu.Item>
-						<Menu.Item name='chairs'>
-							Chairs
-						</Menu.Item>
-						<Menu.Item name='lamps'>
-							Lamps
-						</Menu.Item>
+						{ menuItemList }
 					</Sidebar>
 					<Sidebar.Pusher>
 						<div className="container">
 							<div className="five column centered">
-								<Catalog onAdded={this.onAdded} onRemoved={this.onRemoved}/>
+								<Catalog onAdded={this.onAdded} onRemoved={this.onRemoved} selectedCategoryId={selectedCategoryId}/>
 							</div>
 						</div>
 					</Sidebar.Pusher>
